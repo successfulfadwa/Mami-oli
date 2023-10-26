e5bc1c4959db3a159f9dfe290e9ac9fad8791abf
successfulfadwa <fadwa.aljaoui@gmail.com>
2023-10-26 12:00:30 -0400

    gallery  phone

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const expand = (item, i) => {
   // Check for phone view
   if (window.innerWidth < 768) {
 	gsap.to(items, {
-	  width: item.clicked ? '22vw' : '8vw',
+	  width: item.clicked ? '5vw' : '5vw',
 	  duration: 2.5,
 	  ease: 'elastic(1, .3)'
 	});
@@ -33,7 +33,7 @@ const expand = (item, i) => {
 	// Check for phone view
 	if (window.innerWidth < 768) {
 	  gsap.to(item, {
-		width: item.clicked ? '70vw' : '9vw',
+		width: item.clicked ? '42vw' : '70vw',
 		duration: 2.5,
 		ease: 'elastic(1, .3)'
 	  });
